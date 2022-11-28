fcaa4fbc9217eb9d9bb969f79ca9f68e18044cb4
jylee <jyl9311@gmail.com>
2022-11-28 13:44:38 +0900

    commit

---
+++ src/test/java/com/example/demo/MemberServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
---
+++ src/test/java/com/example/demo/OrderServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+import com.example.demo.order.Order;
+import com.example.demo.order.OrderService;
+import com.example.demo.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
