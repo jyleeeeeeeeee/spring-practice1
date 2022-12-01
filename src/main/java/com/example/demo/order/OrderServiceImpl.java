68f361de7ae41f7f1467beea74e647185e878cb8
jylee <jyl9311@gmail.com>
2022-12-01 14:49:11 +0900

    빈 스코프란?

--- src/main/java/com/example/demo/order/OrderServiceImpl.java
+++ src/main/java/com/example/demo/order/OrderServiceImpl.java
@@ -1,22 +1,23 @@
 package com.example.demo.order;
 
+import com.example.demo.annotation.MainDiscountPolicy;
 import com.example.demo.discount.DiscountPolicy;
 import com.example.demo.discount.FixDiscountPolicy;
 import com.example.demo.member.Member;
 import com.example.demo.member.MemberRepository;
 import com.example.demo.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService{
-    @Autowired
-    private MemberRepository memberRepository;
-    @Autowired
-    private  DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
